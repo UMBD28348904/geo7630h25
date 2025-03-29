2aa8fa43450a0ec188bb40173b2d7a2625ec73ed
UMBD28348904 <umba.dady_dimabanza@courrier.uqam.ca>
2025-03-29 16:46:59 +0000

    update

--- UMBD28348904/TP3/map-control.js
+++ UMBD28348904/TP3/map-control.js
@@ -35,10 +35,12 @@
         data: 'https://donnees.montreal.ca/dataset/9797a946-9da8-41ec-8815-f6b276dec7e9/resource/e18bfd07-edc8-4ce8-8a5a-3b617662a794/download/limites-administratives-agglomeration.geojson'
     };
 
-    var vulnerabiliteSource = {
+    var commercesSource = {
         type: 'geojson',
-        data: 'https://donnees.montreal.ca/fr/dataset/c747ec13-cb20-4fca-b4b1-a316a13a570a/resource/bc77caec-c5ab-4260-ba22-587730ca4378/download/plantation-priorites-polygones-simplifies.geojson'
-    };
+        data: 'https://donnees.montreal.ca/dataset/c1d65779-d3cb-44e8-af0a-b9f2c5f7766d/resource/ece728c7-6f2d-4a51-a36d-21cd70e0ddc7/download/businesses.geojson'
+      };
+
+   
 
     // Définition des couches
 
@@ -56,19 +58,45 @@
         }
     };
 
-    var vulnerabiliteLayer = {
-        id: 'vulnerbialite',
-        type: 'fill',
-        source: 'vulnerabiliteSource',
-        paint: {
-            'fill-color': '#ccc',
-            'fill-opacity': 0.5,
-            'fill-outline-color': '#000'
-        },
-        layout: {
-            'visibility': 'visible' // Affiché par défaut
-        }
-    };
+    // Définition de la couche avec symbologie par type de commerce
+var commercesLayer = {
+    id: 'commerces',
+    type: 'circle',
+    source: 'commercesSource',
+
+    paint: {
+        // Rayon variable selon le type
+        'circle-radius': [
+          'match',
+          ['get', 'type'],
+          'Épicerie', 5,
+          'Pâtisserie/Boulangerie', 7,
+          'Distributrice automatique', 4,
+          'Pharmacie', 6,
+          'Restaurant', 5,
+          3 // taille par défaut
+        ],
+        // Couleur variable selon le type
+        'circle-color': [
+          'match',
+          ['get', 'type'],
+          'Épicerie', 'orange',
+          'Pâtisserie/Boulangerie', 'yellow',
+          'Distributrice automatique', 'blue',
+          'Pharmacie', 'green',
+          'Restaurant', 'purple',
+          'grey' // couleur par défaut
+        ],
+        'circle-stroke-color': '#fff',
+        'circle-stroke-width': 1
+      },  
+      filter: ['==', ['get', 'statut'], 'Ouvert']
+  };
+
+
+    
+
+
   
 
 
