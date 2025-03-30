40a1c7cd4c29bd79554fc904edb96251ffe32df2
UMBD28348904 <umba.dady_dimabanza@courrier.uqam.ca>
2025-03-30 15:43:27 +0000

    valider

--- UMBD28348904/TP3/map-control.js
+++ UMBD28348904/TP3/map-control.js
@@ -1,75 +1,98 @@
+// création de la carte Mapbox GL
 var map = new maplibregl.Map({
-    container: 'map',
-    style: 'https://api.maptiler.com/maps/dataviz/style.json?key=JhO9AmIPH59xnAn5GiSj',
-    center: [-73.55, 45.55],
-    zoom: 10,
-    hash: true
+    container: 'map', // identifiant de l'élément HTML conteneur de la carte
+    style: 'https://api.maptiler.com/maps/dataviz/style.json?key=JhO9AmIPH59xnAn5GiSj', // URL du style de la carte
+    center: [-73.55, 45.55], // position centrale de la carte
+    zoom: 9, // niveau de zoom initial
+    hash: true // activation du hash pour la gestion de l'historique de la carte
 });
 
-map.addControl(new maplibregl.NavigationControl());
-map.addControl(new maplibregl.AttributionControl({ compact: true }));
+var nav = new maplibregl.NavigationControl({
+    showCompass: true,
+    showZoom: true,
+    visualizePitch: true
+});
+map.addControl(nav, 'top-right');
+
+var geolocateControl = new maplibregl.GeolocateControl({
+    positionOptions: { enableHighAccuracy: true },
+    trackUserLocation: true
+});
+map.addControl(geolocateControl, 'bottom-right');
 
 var scale = new maplibregl.ScaleControl({ unit: 'metric' });
 map.addControl(scale);
 
-const inondationdropdown = document.getElementById('inondation-dropdown');
-setTimeout(() => { inondationdropdown.disabled = false; }, 1000);
 
-var arrondissementsSource = {
+
+
+// Récupération de la liste déroulante
+const inondationdropdown = document.getElementById('inondation-dropdown');
+
+// activation de la liste déroulante (à adapter selon votre logique)
+setTimeout(() => {inondationdropdown.disabled = false;}, 1000); // Simule un chargement avant d'activer
+
+
+const arrondissementsSource = {
     type: 'geojson',
     data: 'https://donnees.montreal.ca/dataset/9797a946-9da8-41ec-8815-f6b276dec7e9/resource/e18bfd07-edc8-4ce8-8a5a-3b617662a794/download/limites-administratives-agglomeration.geojson'
 };
 
-var vulnerabiliteSource = {
+const vulnerabiliteSource = {
     type: 'geojson',
     data: 'https://donnees.montreal.ca/dataset/3603f75a-1963-4130-9fc5-ab3e7272211a/resource/01afc867-11f2-4a3b-b77e-d5e9ee853c87/download/vulnerabilite-crues-polygones-simplifies-2022.geojson'
 };
 
-var arrondissementsLayer = {
-    id: 'arrondissements',
-    type: 'fill',
-    source: 'arrondissementsSource',
-    paint: {
-        'fill-color': '#ccc',
-        'fill-opacity': 0.5,
-        'fill-outline-color': '#000'
-    },
-    layout: { 'visibility': 'visible' }
-};
+map.on('load', function () {
+    map.addSource('arrondissements', arrondissementsSource);
+    map.addLayer({
+        id: 'arrondissements',
+        type: 'fill',
+        source: 'arrondissements',
+        paint: {
+            'fill-color': '#ccc',
+            'fill-opacity': 0.5,
+            'fill-outline-color': '#000'
+        },
+        layout: { 'visibility': 'visible' }
+    });
 
-var vulnerabiliteLayer = {
-    id: 'vulnerabilite',
-    type: 'fill',
-    source: 'vulnerabiliteSource',
-    paint: {
-        'fill-color': [
-          'match',
-          ['get', 'CruesCat'],
-          'Non significative', 'orange',
-          'Mineure', 'yellow',
-          'Modéré', 'blue',
-          'Élevée', 'green',
-          'Majeure', 'purple',
-          'S.O.', '#ec7063',
-          'grey'
-        ],
-        'fill-outline-color': '#fff',
-        'fill-opacity': 0.7
-    }, 
-    layout: { 'visibility': 'visible' }
-};
+    map.addSource('vulnerabilite', vulnerabiliteSource);
+    map.addLayer({
+        id: 'vulnerabilite',
+        type: 'fill',
+        source: 'vulnerabilite',
+        paint: {
+            'fill-color': [
+                'match', ['get', 'CruesCat'],
+                'Non significative', 'orange',
+                'Mineure', 'yellow',
+                'Modéré', 'blue',
+                'Élevée', 'green',
+                'Majeure', 'purple',
+                'S.O.', '#ec7063',
+                'grey'
+            ],
+            'fill-outline-color': '#fff',
+            'fill-opacity': 0.7
+        },
+        layout: { 'visibility': 'visible' }
+    });
+    generateLegend();
+});
+
+const categories = [
+    { label: 'Non significative', color: 'orange' },
+    { label: 'Mineure', color: 'yellow' },
+    { label: 'Modéré', color: 'blue' },
+    { label: 'Élevée', color: 'green' },
+    { label: 'Majeure', color: 'purple' },
+    { label: 'S.O.', color: '#ec7063' }
+];
 
 function generateLegend() {
     const legend = document.getElementById('legend');
     legend.innerHTML = '';
-    const categories = [
-        { label: 'Non significative', color: 'orange' },
-        { label: 'Mineure', color: 'yellow' },
-        { label: 'Modéré', color: 'blue' },
-        { label: 'Élevée', color: 'green' },
-        { label: 'Majeure', color: 'purple' },
-        { label: 'S.O.', color: '#ec7063' }
-    ];
 
     categories.forEach(category => {
         const legendItem = document.createElement('div');
@@ -78,7 +101,6 @@ function generateLegend() {
         const colorBox = document.createElement('div');
         colorBox.classList.add('legend-color');
         colorBox.style.backgroundColor = category.color;
-        colorBox.dataset.value = category.label;
 
         const label = document.createElement('span');
         label.classList.add('legend-label');
@@ -87,7 +109,7 @@ function generateLegend() {
         legendItem.appendChild(colorBox);
         legendItem.appendChild(label);
         legendItem.addEventListener('click', function () {
-            inondationdropdown.value = category.label;
+            inondationDropdown.value = category.label;
             updateMapLayer();
         });
 
@@ -96,25 +118,17 @@ function generateLegend() {
 }
 
 function updateMapLayer() {
-    const selectedCategory = inondationdropdown.value;
-    if (selectedCategory) {
-        map.setFilter('vulnerabilite', ['==', ['get', 'CruesCat'], selectedCategory]);
-    } else {
-        map.setFilter('vulnerabilite', null);
-    }
+    const selectedCategory = inondationDropdown.value;
+    map.setFilter('vulnerabilite', selectedCategory ? ['==', ['get', 'CruesCat'], selectedCategory] : null);
 }
 
-inondationdropdown.addEventListener('change', updateMapLayer);
+inondationDropdown.addEventListener('change', updateMapLayer);
 
 document.getElementById('neighborhoods').addEventListener('change', function () {
-    const visibility = this.checked ? 'visible' : 'none';
-    map.setLayoutProperty('arrondissements', 'visibility', visibility);
+    map.setLayoutProperty('arrondissements', 'visibility', this.checked ? 'visible' : 'none');
 });
 
-map.on('load', function () {
-    map.addSource('arrondissementsSource', arrondissementsSource);
-    map.addLayer(arrondissementsLayer);
-    map.addSource('vulnerabiliteSource', vulnerabiliteSource);
-    map.addLayer(vulnerabiliteLayer);
-    generateLegend();
+// Appliquer le filtre lors de la sélection dans le dropdown
+inondationDropdown.addEventListener('change', function () {
+    updateMapLayer();
 });
