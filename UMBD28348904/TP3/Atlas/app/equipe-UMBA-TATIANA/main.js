e769a9e41442209bf205f20d66a9b08815d42a02
UMBD28348904 <umba.dady_dimabanza@courrier.uqam.ca>
2025-04-19 14:21:00 +0000

    update

--- UMBD28348904/TP3/Atlas/app/equipe-UMBA-TATIANA/main.js
+++ UMBD28348904/TP3/Atlas/app/equipe-UMBA-TATIANA/main.js
@@ -2,29 +2,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Initialisation de la carte MapLibre GL
     const map = new maplibregl.Map({
-        container: 'map',
-        style: {
-            'version': 8,
-            'sources': {
-                'esri-light-gray': {
-                    'type': 'raster',
-                    'tiles': [
-                        'https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}'
-                    ],
-                    'tileSize': 256,
-                    'attribution': 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ'
-                }
-            },
-            'layers': [
-                {
-                    'id': 'esri-light-gray',
-                    'type': 'raster',
-                    'source': 'esri-light-gray'
-                }
-            ]
-        },
-        center: [-73.55, 45.55], // Coordonnées de Montréal [longitude, latitude]
-        zoom: 10
+        container: 'map', // identifiant de l'élément HTML conteneur de la carte
+        style: 'https://api.maptiler.com/maps/dataviz/style.json?key=JhO9AmIPH59xnAn5GiSj', // URL du style de la carte
+        center: [-73.55, 45.55], // position centrale de la carte
+        zoom: 11, // niveau de zoom initial
+        hash: true // activation du hash pour la gestion de l'historique de la carte
     });
 
     // Ajouter les contrôles de navigation (zoom, rotation)
@@ -60,10 +42,31 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     };
 
+
+
+      //Labels des arrondissements
+    var arrondissementsLabelsLayer = {
+        id: 'arrondissements-labels',
+        type: 'symbol',
+        source: 'arrondissementsSource',
+        layout: {
+          'text-field': ['get', 'NOM'], 
+          'text-font': ['Open Sans Bold', 'Arial Unicode MS Bold'],
+          'text-size': 14,
+          'text-anchor': 'center'
+        },
+        paint: {
+          'text-color': '#111',
+          'text-halo-color': '#fff',
+          'text-halo-width': 2
+        }
+      };
+
     // Ajout de la source et couche des arrondissements
     map.on('load', function () {
         map.addSource('arrondissementsSource', arrondissementsSource);
         map.addLayer(arrondissementsLayer);
+        map.addLayer(arrondissementsLabelsLayer);
     });
 
     // Gestion de la case à cocher pour afficher/masquer les arrondissements
